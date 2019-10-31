781e9e165e4149bac5313b5c37bbcb69c0332de2
James Starkie <jamesstarkie@rideways.com>
2019-10-31 12:06:08 +0000

    Generate component files for icons

--- packages/greenhouse/scripts/build.js
+++ packages/greenhouse/scripts/build.js
@@ -1,19 +1,44 @@
-const fs = require('fs');
 const path = require('path');
-const glob = require('glob'); // eslint-disable-line
+const fs = require('fs-extra');
+const glob = require('fast-glob');
+const icons = require('@mdi/js');
 
-glob('./src/**/*.svelte', (err, files) => {
-  let output = '';
+// Create dist directory
+if (fs.existsSync('dist')) {
+  fs.removeSync('dist/');
+}
+fs.mkdirSync('dist/components', { recursive: true });
 
-  files.forEach(file => {
-    output += `import ${path.basename(file, '.svelte')} from '${file}';\n`;
+// Create icon components in dist
+Object.keys(icons)
+  .filter(iconName => !iconName.startsWith('__'))
+  .forEach(iconName => {
+    const output = `<svg class="icon" viewBox="0 0 24 24">
+  <path d="${icons[iconName]}" />
+</svg>`;
+
+    fs.writeFileSync(`dist/components/${iconName}.svelte`, output);
   });
 
-  const components = files
-    .map(file => path.basename(file, '.svelte'))
-    .join(',\n  ');
-
-  output += `\nexport {\n  ${components}\n};\n`;
-
-  fs.writeFileSync('index.js', output);
+// Copy components to dist
+const components = glob.sync('src/**/*.svelte');
+components.forEach(component => {
+  const fileName = path.basename(component);
+  fs.copyFileSync(component, `dist/components/${fileName}`);
 });
+
+// Generate entry file
+const files = glob.sync('dist/components/*.svelte');
+let output = '';
+
+files.forEach(file => {
+  output += `import ${path.basename(file, '.svelte')} from './${file}';\n`;
+});
+
+const componentList = files
+  .map(file => path.basename(file, '.svelte'))
+  .join(',\n  ');
+
+output += `\nexport {\n  ${componentList}\n};\n`;
+
+fs.writeFileSync('index.js', output);
