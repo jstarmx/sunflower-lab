52ca2729146f60716ad824d4f6d44a2646b6df2f
James Starkie <jstar.mx@gmail.com>
2021-10-10 21:15:01 +0100

    Tweak storybook webpack config for svelte

--- packages/greenhouse/.storybook/main.js
+++ packages/greenhouse/.storybook/main.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 module.exports = {
   core: {
     builder: 'webpack5',
   },
   stories: ['../src/**/*.stories.@(ts|svelte)'],
-  addons: ['@storybook/addon-svelte-csf']
+  addons: ['@storybook/addon-svelte-csf'],
+  webpackFinal: async (config) => {
+    config.resolve.alias.svelte = path.resolve('node_modules', 'svelte');
+    return config;
+  },
 };
