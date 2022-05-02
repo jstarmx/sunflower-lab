533dac82af76d9a27b5b790c14d807b32e18f63d
James Starkie <james@starkie.dev>
2022-05-02 23:27:52 +0100

    fix storybook build

--- packages/greenhouse/.storybook/main.cjs
+++ packages/greenhouse/.storybook/main.cjs
@@ -1,4 +1,15 @@
 const path = require('path');
+const ResolveTypeScriptPlugin = require('resolve-typescript-plugin').default;
+
+const replaceFileExtension = (filePath, newExtension) => {
+  const { name, root, dir } = path.parse(filePath);
+  return path.format({
+    name,
+    root,
+    dir,
+    ext: newExtension,
+  });
+};
 
 module.exports = {
   core: {
@@ -8,6 +19,37 @@ module.exports = {
   addons: ['@storybook/addon-svelte-csf'],
   webpackFinal: async (config) => {
     config.resolve.alias.svelte = path.resolve('node_modules', 'svelte');
+
+    // Find the plugin instance that needs to be mutated
+    const virtualModulesPlugin = config.plugins.find(
+      (plugin) => plugin.constructor.name === 'VirtualModulesPlugin',
+    );
+
+    // Change the file extension to .cjs for all files that end with "generated-stories-entry.js"
+    virtualModulesPlugin._staticModules = Object.fromEntries(
+      Object.entries(virtualModulesPlugin._staticModules).map(
+        ([key, value]) => {
+          if (key.endsWith('generated-stories-entry.js')) {
+            return [replaceFileExtension(key, '.cjs'), value];
+          }
+          return [key, value];
+        },
+      ),
+    );
+
+    // Change the entry points to point to the appropriate .cjs files
+    config.entry = config.entry.map((entry) => {
+      if (entry.endsWith('generated-stories-entry.js')) {
+        return replaceFileExtension(entry, '.cjs');
+      }
+      return entry;
+    });
+
+    config.resolve = {
+      ...config.resolve,
+      plugins: [new ResolveTypeScriptPlugin()],
+    };
+
     return config;
   },
 };
